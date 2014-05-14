5cbf46aef4eafee98692ae96a73407d6c4ecf218
fernandoescolar <fer.escolar@gmail.com>
2014-05-14 19:01:45 +0200

    item type functions fixed and replaced

--- ItemUpdater.cs
+++ ItemUpdater.cs
@@ -1,11 +1,20 @@
 namespace GildedRose
 {
-    internal class ItemUpdater<T> where T:Item
+    internal class ItemUpdater<T> where T : Item
     {
-       
+        private static bool IsSulfuras(Item item)
+        {
+            return item.Name == "Sulfuras, Hand of Ragnaros";
+        }
+
         private static bool IsAgedBrie(Item item)
         {
             return item.Name == "Aged Brie";
+        }
+
+        private static bool IsBackstage(Item item)
+        {
+            return item.Name == "Backstage passes to a TAFKAL80ETC concert";
         }
 
         private static bool IsExpired(Item item)
@@ -15,22 +24,17 @@ namespace GildedRose
 
         private static void DecreaseSellIn(Item item)
         {
-            if (item.Name != "Sulfuras, Hand of Ragnaros")
+            if (!IsSulfuras(item))
             {
                 item.SellIn--;
             }
-        }
-
-        private static bool IsBackstage(Item item)
-        {
-            return item.Name == "Backstage passes to a TAFKAL80ETC concert";
         }
 
         private static void DecreaseQuality(Item item)
         {
             if (!HasQuality(item)) return;
 
-            if (item.Name != "Sulfuras, Hand of Ragnaros")
+            if (!IsSulfuras(item))
             {
                 item.Quality--;
             }
@@ -51,7 +55,7 @@ namespace GildedRose
 
         private static bool IsExpirable(Item item)
         {
-            return (item.Name != "Aged Brie" && item.Name != "Backstage passes to a TAFKAL80ETC concert");
+            return (!IsAgedBrie(item) && !IsBackstage(item));
         }
 
         public static void UpdateQuality(T item)
@@ -85,10 +89,10 @@ namespace GildedRose
             if (IsExpirable(item))
             {
                 DecreaseQuality(item);
-            return;
+                return;
             }
 
-    if (IsAgedBrie(item))
+            if (IsAgedBrie(item))
             {
                 IncreaseQuality(item);
                 return;
@@ -99,7 +103,6 @@ namespace GildedRose
                 item.Quality = 0;
                 return;
             }
-            
         }
 
     }
