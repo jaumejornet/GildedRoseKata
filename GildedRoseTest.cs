82d8d5aa8eb91da4f5e4374a5c0170abdb3145fe
fernandoescolar <fer.escolar@gmail.com>
2014-05-14 18:48:58 +0200

    refactoring tests and adding sulfuras test cases

--- GildedRoseTest.cs
+++ GildedRoseTest.cs
@@ -1,112 +1,82 @@
-using System;
 using NUnit.Framework;
 using System.Collections.Generic;
 
 namespace GildedRose
 {
-    [TestFixture()]
+    [TestFixture]
     public class GildedRoseTest
-    {        
-        [Test()]
-        public void Test_Baja_SellIn()
-        {
-            const int actualSellin = 5;
-            const int expectedSellin = 4;
+    {
+        private const string DefaultItemName = "foo";
+        private const string SulfurasItemName = "Sulfuras, Hand of Ragnaros";
+        private const string AgedBrieItemName = "Aged Brie";
+        private const string BackstageItemName = "Backstage passes to a TAFKAL80ETC concert";
 
-            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = actualSellin, Quality = 5 } };
-            GildedRose app = new GildedRose(Items);
-            app.UpdateQuality();
-            Assert.AreEqual(expectedSellin, Items[0].SellIn);
+        [TestCase(5, 4, TestName = "SellInShouldDecreased")]
+        public void SellInChanges(int actualSellin, int expectedSellin)
+        {
+            AssertSellInChange(DefaultItemName, actualSellin, expectedSellin, 5);
         }
 
-        [TestCase(5, 4)]
-        [TestCase(0, 0)]
-        public void Test_ItemsQualityShouldDecrease(int initialQ, int expectedQ)
+        [TestCase(4, 5, 4, TestName = "QualityShouldDecrease")]
+        [TestCase(4, 0, 0, TestName = "QualityShouldNotBeNegative")]
+        [TestCase(0, 3, 1, TestName = "QualityShouldDecreaseFasterWhenSellInIsZero")]
+        public void QualityChanges(int actualSellin, int initialQuality, int expectedQuality)
         {
-
-            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 4, Quality = initialQ } };
-            GildedRose app = new GildedRose(Items);
-            app.UpdateQuality();
-            Assert.AreEqual(expectedQ, Items[0].Quality);
+            AssertQualityChange(DefaultItemName, actualSellin, initialQuality, expectedQuality);
         }
 
-        [TestCase(0, 3, 1)]
-        public void Test_ItemsQualityShouldDecreaseFaster(int actualS, int initialQ, int expectedQ)
+        [TestCase(5, 5, TestName = "SulfurasSellInIsNeverDecreased")]
+        public void SulfurasSellInChanges(int actualSellin, int expectedSellin)
         {
-
-            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = actualS, Quality = initialQ } };
-            GildedRose app = new GildedRose(Items);
-            app.UpdateQuality();
-            Assert.AreEqual(expectedQ, Items[0].Quality);
+            AssertSellInChange(SulfurasItemName, actualSellin, expectedSellin, 5);
         }
 
-        [TestCase(0, 3, 2)]
-        public void Test_ItemsQualityShouldDecreaseFaster_Negative(int actualS, int initialQ, int expectedQ)
+        [TestCase(1, 10, 10, TestName = "SulfurasQualityIsNeverDecreased")]
+        public void SulfurasQualityChanges(int actualSellin, int initialQuality, int expectedQuality)
         {
-
-            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = actualS, Quality = initialQ } };
-            GildedRose app = new GildedRose(Items);
-            app.UpdateQuality();
-            Assert.AreNotEqual(expectedQ, Items[0].Quality);
+            AssertQualityChange(SulfurasItemName, actualSellin, initialQuality, expectedQuality);
         }
 
-
-        [TestCase(1, 2)]
-        [TestCase(50, 50)]
-        public void Test_ItemsQualityShouldIncrease(int initialQ, int expectedQ)
+        [TestCase(1, 1, 2, TestName = "AgedBrieQualityShouldIncrease")]
+        [TestCase(1, 50, 50, TestName = "AgedBrieQualityShouldNotBeGreaterThan50")]
+        public void AgedBrieQualityChanges(int actualSellin, int initialQuality, int expectedQuality)
         {
-            int actualS = 1;
-
-            IList<Item> Items = new List<Item> { new Item { Name = "Aged Brie", SellIn = actualS, Quality = initialQ } };
-            GildedRose app = new GildedRose(Items);
-            app.UpdateQuality();
-            Assert.AreEqual(expectedQ, Items[0].Quality);
+            AssertQualityChange(AgedBrieItemName, actualSellin, initialQuality, expectedQuality);
         }
 
-        //“Backstage passes”, like aged brie, increases in Quality as it’s SellIn value approaches; 
-        // Quality increases by 2 when there are 10 days or less and by 3 when there are 5 days or 
-        // less but Quality drops to 0 after the concert 
-        
-        [TestCase(10, 12, 10)]
-        [TestCase(10, 12, 8)]  
-        public void Test_BackStageShouldIncreaseQualityByTwoWhenTenOrLessDaysSellIn(int initialQuality, int expectedQuality, int sellIn)
+        [TestCase(10, 10, 12, TestName = "BackStageShouldIncreaseQualityByTwoWhenTenDaysSellIn")]
+        [TestCase(8, 10, 12, TestName = "BackStageShouldIncreaseQualityByTwoWhenLessThanTenDaysSellIn")]
+        [TestCase(5, 10, 13, TestName = "BackStageShouldIncreaseQualityByThreeWhenFiveDaysSellIn")]
+        [TestCase(3, 10, 13, TestName = "BackStageShouldIncreaseQualityByThreeWhenLessThanFiveDaysSellIn")]
+        [TestCase(0, 10, 0, TestName = "BackStageShouldSetQualityToZeroWhenSellInIsZero")]
+        [TestCase(12, 50, 50, TestName = "BackStageQualityShouldNotBeGreaterThan50")]
+        [TestCase(8, 50, 50, TestName = "BackStageQualityShouldNotBeGreaterThan50EvenWhenLessThanTenDaysSellIn")]
+        [TestCase(3, 50, 50, TestName = "BackStageQualityShouldNotBeGreaterThan50EvenWhenLessThanFiveDaysSellIn")]
+        public void BackstageQualityChanges(int actualSellin, int initialQuality, int expectedQuality)
         {
-            IList<Item> Items = new List<Item> { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = sellIn, Quality = initialQuality } };
-            GildedRose app = new GildedRose(Items);
-            app.UpdateQuality();
-            Assert.AreEqual(expectedQuality, Items[0].Quality);
+            AssertQualityChange(BackstageItemName, actualSellin, initialQuality, expectedQuality);
         }
 
-        [TestCase(10, 13, 5)]
-        [TestCase(10, 13, 3)]
-        public void Test_BackStageShouldIncreaseQualityByThreeWhenFiveOrLessDaysSellIn(int initialQuality, int expectedQuality, int sellIn)
+        private static void AssertQualityChange(string itemName, int actualSellin, int initialQuality, int expectedQuality)
         {
-            IList<Item> Items = new List<Item> { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = sellIn, Quality = initialQuality } };
-            GildedRose app = new GildedRose(Items);
-            app.UpdateQuality();
-            Assert.AreEqual(expectedQuality, Items[0].Quality);
+            var items = ArangeAndActGildedRose(itemName, actualSellin, initialQuality);
+            Assert.AreEqual(expectedQuality, items[0].Quality);
         }
 
-        [TestCase(10, 0, 0)]
-        public void Test_BackStageShouldSetQualityToZeroWhenSellInIsZero(int initialQuality, int expectedQuality, int sellIn)
+        private static void AssertSellInChange(string itemName, int actualSellin, int expectedSellin, int initialQuality)
         {
-            IList<Item> Items = new List<Item> { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = sellIn, Quality = initialQuality } };
-            GildedRose app = new GildedRose(Items);
-            app.UpdateQuality();
-            Assert.AreEqual(expectedQuality, Items[0].Quality);
+            var items = ArangeAndActGildedRose(itemName, actualSellin, initialQuality);
+            Assert.AreEqual(expectedSellin, items[0].SellIn);
         }
 
-       
-        // todo borrar
-        //public void Test_ItemsQualityShouldIncreaseWhenSellInIsLessZero(int initialQ, int expectedQ, int sellIn)
-        //{
-            
-        //    IList<Item> Items = new List<Item> { new Item { Name = "Aged Brie", SellIn = sellIn, Quality = initialQ } };
-        //    GildedRose app = new GildedRose(Items);
-        //    app.UpdateQuality();
-        //    Assert.AreEqual(expectedQ, Items[0].Quality);
-        //}
- 
+        private static List<Item> ArangeAndActGildedRose(string itemName, int actualSellin, int initialQuality)
+        {
+            var items = new List<Item> { new Item { Name = itemName, SellIn = actualSellin, Quality = initialQuality } };
+            var app = new GildedRose(items);
+            app.UpdateQuality();
+
+            return items;
+        }
     }
 }
 
